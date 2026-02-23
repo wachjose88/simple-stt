f37b45215bbaad7de7960a6113be2071dc8e474c
Josef Wachtler <josef.wachtler@gmail.com>
2026-02-23 17:51:49 +0100

    select model

--- dictation/editor.py
+++ dictation/editor.py
@@ -2,40 +2,30 @@ import logging
 import re
 
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QTextEdit, QGridLayout
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QTextEdit, QGridLayout, QComboBox
 
 logger = logging.getLogger('dictation.app')
 
 
-class Editor(QWidget):
+class ActionPanel(QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self: QWidget, parent: QWidget, text_edit: QTextEdit):
         super().__init__(parent)
-        self.start_text = self.tr('Start')
-        self.stop_text = self.tr('Stop')
-        self.vbox = QVBoxLayout()
-        self.hbox = QHBoxLayout()
-        self.start_stop_button = QPushButton(self.start_text)
-        self.start_stop_button.clicked.connect(self.start_stop_button_clicked)
-        self.hbox.addWidget(self.start_stop_button)
-        self.copy_button = QPushButton(self.tr('Copy'))
-        self.copy_button.clicked.connect(self.copy_button_clicked)
-        self.hbox.addWidget(self.copy_button)
-        self.vbox.addLayout(self.hbox)
+        self.text_edit = text_edit
 
         self.actions = {
-            self.tr('Period .'): '.',
-            self.tr('Comma ,'): ',',
-            self.tr('Colon :'): ':',
-            self.tr('Semicolon ;'): ';',
-            self.tr('Question Mark ?'): '?',
-            self.tr('Exclamation Mark !'): '!',
+            self.tr('Period .'): '. ',
+            self.tr('Comma ,'): ', ',
+            self.tr('Colon :'): ': ',
+            self.tr('Semicolon ;'): '; ',
+            self.tr('Question Mark ?'): '? ',
+            self.tr('Exclamation Mark !'): '! ',
             self.tr('Open Bracket ('): '(',
-            self.tr('Closed Bracket )'): ')',
+            self.tr('Closed Bracket )'): ') ',
             self.tr('Open Square Bracket ['): '[',
-            self.tr('Closed Square Bracket ]'): ']',
+            self.tr('Closed Square Bracket ]'): '] ',
             self.tr('Open Curly Bracket {'): '{',
-            self.tr('Closed Curly Bracket }'): '}',
+            self.tr('Closed Curly Bracket }'): '} ',
         }
 
         self.action_grid = QGridLayout()
@@ -51,19 +41,13 @@ class Editor(QWidget):
             col = int(i % 2)
             self.action_grid.addWidget(action_button, row, col)
             i = i + 1
-
-        self.text_hbox = QHBoxLayout()
-        self.text_edit = QTextEdit()
-        self.text_hbox.addWidget(self.text_edit)
-        self.text_hbox.addLayout(self.action_grid)
-        self.vbox.addLayout(self.text_hbox)
-        self.setLayout(self.vbox)
+        self.setLayout(self.action_grid)
 
     def add_to_text(self):
         text = self.sender().text()
         text = self.actions[text]
         logger.debug(f'Add text {text}')
-        self.text_edit.insertPlainText(f'{text} ')
+        self.text_edit.insertPlainText(text)
         self.text_edit.setFocus()
 
     def add_nl_to_text(self):
@@ -71,13 +55,50 @@ class Editor(QWidget):
         self.text_edit.insertPlainText('\n')
         self.text_edit.setFocus()
 
+
+class Editor(QWidget):
+
+    def __init__(self, parent, models):
+        super().__init__(parent)
+        self.models = models
+        self.start_text = self.tr('Start')
+        self.stop_text = self.tr('Stop')
+        self.vbox = QVBoxLayout()
+
+        self.hbox = QHBoxLayout()
+        self.select_model = QComboBox()
+        for model in self.models:
+            self.select_model.addItem(model)
+        self.select_model.currentTextChanged.connect(self.select_model_changed)
+        self.hbox.addWidget(self.select_model)
+        self.start_stop_button = QPushButton(self.start_text)
+        self.start_stop_button.clicked.connect(self.start_stop_button_clicked)
+        self.hbox.addWidget(self.start_stop_button)
+        self.copy_button = QPushButton(self.tr('Copy'))
+        self.copy_button.clicked.connect(self.copy_button_clicked)
+        self.hbox.addWidget(self.copy_button)
+        self.vbox.addLayout(self.hbox)
+
+        self.text_hbox = QHBoxLayout()
+        self.text_edit = QTextEdit()
+        self.text_hbox.addWidget(self.text_edit)
+        self.action_panel = ActionPanel(self, self.text_edit)
+        self.text_hbox.addWidget(self.action_panel)
+        self.vbox.addLayout(self.text_hbox)
+        self.setLayout(self.vbox)
+
+    def select_model_changed(self, text):
+        logger.debug(f'Selected model {text}')
+
     def start_stop_button_clicked(self):
         logger.debug('Start stop button')
         label = self.start_stop_button.text()
         if label == self.start_text:
             self.start_stop_button.setText(self.stop_text)
+            self.select_model.setDisabled(True)
         else:
             self.start_stop_button.setText(self.start_text)
+            self.select_model.setDisabled(False)
 
     def copy_button_clicked(self):
         logger.debug('Copy button')
--- dictation/main.py
+++ dictation/main.py
@@ -1,6 +1,9 @@
+
 import locale
 import logging.config
 import sys
+from argparse import ArgumentParser, ArgumentTypeError
+from pathlib import Path
 
 from PySide6.QtCore import QTranslator, QLibraryInfo
 from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox
@@ -14,13 +17,17 @@ logger = logging.getLogger('dictation.app')
 
 class DictationApp(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, models_directory):
         super().__init__()
         logger.debug('Initializing Dictionation App')
         self.setWindowTitle(self.tr('Dictionation'))
         self.statusBar().showMessage(self.tr('Dictionation App Started'))
 
-        self.editor = Editor(self)
+        self.models_directory = models_directory
+        self.models = {str(x.name): x for x in self.models_directory.iterdir() if x.is_dir()}
+        logger.debug(self.models)
+
+        self.editor = Editor(self, self.models)
         self.setCentralWidget(self.editor)
         self.show()
 
@@ -36,8 +43,29 @@ class DictationApp(QMainWindow):
             event.accept()
 
 
+def path_type(arg):
+    logger.debug(arg)
+    try:
+        path = Path(arg).resolve(strict=True)
+        logger.debug(path)
+        return path
+    except OSError:
+        raise ArgumentTypeError()
+
+
 if __name__ == '__main__':
     logging.config.dictConfig(LOGGING_CONFIG)
+
+    parser = ArgumentParser(
+        prog='main.py',
+        description='A very simple speech to text application.'
+    )
+    parser.add_argument('-md', '--models-directory',
+                        type=path_type,
+                        help='Directory containing vosk models',
+                        required=True)
+    args = parser.parse_args()
+
     app = QApplication(sys.argv)
     qtbase_translator = QTranslator()
     qtbase_translator.load(
@@ -52,5 +80,6 @@ if __name__ == '__main__':
     translator = QTranslator()
     translator.load('locales/dictation_' + locale.getlocale()[0][0:2])
     app.installTranslator(translator)
-    dapp = DictationApp()
+
+    dapp = DictationApp(args.models_directory)
     sys.exit(app.exec())
